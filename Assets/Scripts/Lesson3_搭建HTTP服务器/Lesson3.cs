51ba30645db14ce192b70a550aaf7348706681f8
lqd <2448217643@qq.com>
2023-12-22 11:35:11 +0800

    全部文件

---
+++ Assets/Scripts/Lesson3_搭建HTTP服务器/Lesson3.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region 知识点一 搭建HTTP服务器的几种方式
+        //1.使用别人做好的HTTP服务器软件，一般作为资源服务器时使用该方式（学习阶段建议使用）
+        //2.自己编写HTTP服务器应用程序，一般作为Web服务器 或者 短链接游戏服务器时 使用该方式
+        #endregion
+
+        #region 知识点二 使用别人做好的HTTP服务器软件来搭建HTTP资源服务器
+        //下载hfs等HTTP服务器软件
+        //在想要作为HTTP资源服务器的电脑上运行它
+        #endregion
+
+        #region 知识点三 使用别人做好的Web服务器进行测试
+        //我们在学习过程中，可以直接在别人做好的Web服务器上获取信息和资源
+        //比如我们可以下载任意网站上可被下载的图片
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
